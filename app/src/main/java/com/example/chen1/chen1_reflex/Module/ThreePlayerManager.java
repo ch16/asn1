50f462e4f19190d8f26fcef2299622a3ac9d2024
Hong Chen <chenhong1211@gmail.com>
2015-10-05 11:05:19 -0600

    Update ThreePlayerManager.java

--- app/src/main/java/com/example/chen1/chen1_reflex/Module/ThreePlayerManager.java
+++ app/src/main/java/com/example/chen1/chen1_reflex/Module/ThreePlayerManager.java
@@ -7,12 +7,12 @@ public class ThreePlayerManager {
     public ThreePlayerManager() {
     }
 
-
+    // record the time of each player if the button is clicked
     private double playerOneTime = Double.POSITIVE_INFINITY;
     private double playerTwoTime = Double.POSITIVE_INFINITY;
     private double playerThreeTime = Double.POSITIVE_INFINITY;    
     private int clickedTimes = 0;
-
+    //record how many players have clicked in each round
     private String resultText = "";
 
     public String getText(){
@@ -22,25 +22,26 @@ public class ThreePlayerManager {
     public int getClickedTimes(){
         return clickedTimes;
     }
-
-
-
+    
+    //if player one clicks
     public void notePlayerOne(){
             clickedTimes ++;
             playerOneTime = System.currentTimeMillis();
     }
 
+    //if player two clicks
     public void notePlayerTwo(){
-
             clickedTimes ++;
             playerTwoTime = System.currentTimeMillis();
     }
+    
+    //if player three clicks
     public void notePlayerThree(){
-
             clickedTimes ++;
             playerThreeTime = System.currentTimeMillis();
     }
 
+    //check who clicks first and add it to the statistics list
     public void checkThreeResults() {
         if (playerOneTime != Double.POSITIVE_INFINITY) {
             resultText = resultText + "Player 1 Clicked\n";
@@ -51,7 +52,8 @@ public class ThreePlayerManager {
         if (playerThreeTime != Double.POSITIVE_INFINITY) {
             resultText = resultText + "Player 3 Clicked\n";
         }
-
+        
+        //add the winner to the list and the text to show up in the alert dialog
         if (playerOneTime > playerTwoTime && playerThreeTime > playerTwoTime) {
             resultText = resultText + "\nPlayer 2 Wins!";
             StatisticsListStorage.getThreePlayerStatistics().add(2);
@@ -63,7 +65,8 @@ public class ThreePlayerManager {
             StatisticsListStorage.getThreePlayerStatistics().add(3);
         }
     }
-
+    
+    //clear time recorded for the players for the new round
     public void clearThreeResults(){
         clickedTimes = 0;
         playerOneTime = Double.POSITIVE_INFINITY;
