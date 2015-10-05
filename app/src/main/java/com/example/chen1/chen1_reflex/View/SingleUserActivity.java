1207e5357fd60f5d76606b515bcb5abbd0360f3f
CH16 <chen1@cs.ualberta.ca>
2015-10-05 12:03:56 -0600

    final draft #1

--- app/src/main/java/com/example/chen1/chen1_reflex/View/SingleUserActivity.java
+++ app/src/main/java/com/example/chen1/chen1_reflex/View/SingleUserActivity.java
@@ -44,7 +44,7 @@ public class SingleUserActivity extends Activity {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                         reactionTimer.setStart(true);
-                        reactionTimer.startGame();
+                        reactionTimer.startTimer();
                     }
                 });
         alertDialog.show();
